7c8feb58c0c92355a5e287273c6ece3b03cbf389
경태윤 <xodbsrud320@gmail.com>
2021-11-20 21:11:56 +0900

    review2_2

--- src/com/javaex/practice/Ex16.java
+++ src/com/javaex/practice/Ex16.java
@@ -11,11 +11,11 @@ public class Ex16 {
 		System.out.print("숫자를 입력하세요: ");
 		int num = sc.nextInt();
 		int sum = 0;
-		int x = num%5; int y = num/5;
-		// x: num에서 나머지 값을 빼면 5의 배수를 구하기 쉽다[?:최적의 방법인지]
+		int y = num/5;
+		// x: 아웃 조건 때문에 상관 없는 거였어!
 		// y: 5의 배수의 개수는 몫과 같음
 		
-		for(int i=5; i<=(num-x); i=i+5) {sum = sum+i;}
+		for(int i=5; i<=num; i=i+5) {sum = sum+i;}
 		
 		System.out.println("5의배수의 개수: "+y);
 		System.out.print("5의배수의 합: "+sum);
